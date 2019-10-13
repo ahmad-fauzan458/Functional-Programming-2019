4511f40f70aee627370082f67254b6f2f419eb88
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2019-10-13 17:47:31 +0700

    Add HOF practice number 1.3

--- Latihan 1 - High Order Function/latihanHOF.hs
+++ Latihan 1 - High Order Function/latihanHOF.hs
@@ -7,3 +7,12 @@ length' xs = sum $ map ($1) xs
 addByTwo xs = map (+1) (map (+1) xs)
 -- map (+1) (map (+1) xs) menambahkan angka 2 pada setiap elemen di list xs
 -- Contoh : map (+1) (map (+1) [0,1,2]) = [2,3,4]
+
+-- • iter n f x = f (f (... (f x)))
+-- where f occurs n times on the right-hand side of the equation. For instance, we
+-- should have
+-- iter 3 f x = f (f (f x))
+-- and iter 0 f x should return x.
+iter :: (Integral a) => a -> (b -> b) -> b -> b
+iter 0 _ x = x
+iter n f x = f $ iter (n-1) f x
