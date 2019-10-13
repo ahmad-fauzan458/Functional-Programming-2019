b943e9e3ed0cf9b0ffb920fd628f84e7a07ef959
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2019-10-13 17:46:43 +0700

    Add HOF practice number 1.2

--- Latihan 1 - High Order Function/latihanHOF.hs
+++ Latihan 1 - High Order Function/latihanHOF.hs
@@ -1,2 +1,9 @@
 -- (*) Define the length function using map and sum
 length' xs = sum $ map ($1) xs
+
+-- (*) What does map (+1) (map (+1) xs) do? Can you conclude anything in
+-- general about properties of map f (map g xs), where f and g are arbitrary
+-- functions?
+addByTwo xs = map (+1) (map (+1) xs)
+-- map (+1) (map (+1) xs) menambahkan angka 2 pada setiap elemen di list xs
+-- Contoh : map (+1) (map (+1) [0,1,2]) = [2,3,4]
