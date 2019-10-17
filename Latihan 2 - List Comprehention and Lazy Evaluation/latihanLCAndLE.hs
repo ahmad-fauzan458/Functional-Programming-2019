f7b46c01d37a3bc8c0e3a4ccfd53d0c7b053b191
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2019-10-17 15:15:00 +0700

    Remove redundant sieve []

--- Latihan 2 - List Comprehention and Lazy Evaluation/latihanLCAndLE.hs
+++ Latihan 2 - List Comprehention and Lazy Evaluation/latihanLCAndLE.hs
@@ -29,7 +29,6 @@ permutation xs = [ x:ps | x <- xs, ps <- permutation(xs\\[x])]
 -- algoritma Sieve of Erastothenes.
 primes n = sieve [2,3..n]
     where sieve (x:xs) = x : sieve [ y | y <- xs, y `mod` x /= 0]
-          sieve [] = []
 
 -- 6. Buatlah definisi infinite list dari triple pythagoras. List tersebut terdiri dari element
 -- triple bilangan bulat positif yang mengikut persamaan pythagoras x^2 + y^2 = z^2
